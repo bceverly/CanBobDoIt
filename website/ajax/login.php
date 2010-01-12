2a5ef7a396c74edc700915c13b489da40e31b487
Bryan Everly <bryan@peoplestrategy.com>
2010-01-11 20:17:23 -0500

    Intermediate checkin

--- website/ajax/login.php
+++ website/ajax/login.php
@@ -1,25 +1,37 @@
 <?php
 	// Variables
-	$db_host = 'localhost';
-	$db_user = 'webuser';
-	$db_pass = 'web123';
-	$db_name = 'webdb';
-
-	$user_id   = $_POST['user_id'];
-	$user_pass = $_POST['user_pass'];    
-
+	$user_id   = $_GET['user_id'];
+	$user_pass = $_GET['user_pass'];    
+	$num = 0;
+	
 	// DB
-	$connect = mysql_connect( $db_host, $db_user, $db_pass ) or die( mysql_error() );
-	$connection = $connect;
-
-	mysql_select_db( $db_name, $connect ) or die( mysql_error() );
+	$connect = mysql_connect('127.0.0.1:3306', 'webuser', 'web123');
+	if (!$connect) {
+	    die('Could not connect: ' . mysql_error());
+	}
+	
+	$db_selected = mysql_select_db( 'webdb', $connect );
+	if (!$db_selected)
+	{
+		die('Could not select db: ' . mysql_error());
+	}
 
 	// See if the user is in the database
-	$query = sprintf("SELECT COUNT (*) from user_access where lower(user_id) = lower('%s') and user_pass = '%s'",
+	$query = sprintf("SELECT * from user_access where lower(user_id) = lower('%s') and user_pass = '%s'",
 					mysql_real_escape_string($user_id),
 					mysql_real_escape_string($user_pass));
 
-	$num = mysql_query($query);
+	$result = mysql_query($query) or die('boom!');
+	$num = mysql_num_rows($result);
 	
-	echo "query is " + $query;
+	mysql_close($connect);
+	
+	if ($num > 0)
+	{
+		$result['valid'] = true;
+	} else {
+		$result['valid'] = false;
+	}
+	
+	echo json_encode($result);
 ?>
